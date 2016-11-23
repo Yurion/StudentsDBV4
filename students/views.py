6de598cdfdcd7f09d5b5fd1e140e53a068bc6975
Yuriy <khomykyu@gmail.com>
2016-11-23 22:05:05 +0200

    refactor students_table with {% for %} tag

--- students/views.py
+++ students/views.py
@@ -4,7 +4,24 @@ from django.http import HttpResponse
 
 # Views for students
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+        {'id': 1,
+         'first_name': 'Віталій',
+         'last_name': 'Подоба',
+         'ticket': 235,
+         'image': 'img/me.jpeg'},
+        {'id': 2,
+         'first_name': 'Корост',
+         'last_name': 'Андрій',
+         'ticket': 2123,
+         'image': 'img/piv.png'},
+        {'id': 3,
+         'first_name': 'Притула',
+         'last_name': 'Тарас',
+         'ticket': 5332,
+         'image': 'img/podoba3.jpg'}
+    )
+    return render(request, 'students/students_list.html', {'students': students})
 
 
 def students_add(request):
