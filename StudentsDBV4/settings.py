5db3b720c536937df6fa4fc2867ec22ef4756ed0
Yurion <khomykyu@gmail.com>
2016-11-25 12:29:19 +0200

    change PORTAL_URL absolute path approach to HttpRequest.get_raw_uri()

--- StudentsDBV4/settings.py
+++ StudentsDBV4/settings.py
@@ -120,6 +120,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
-PORTAL_URL = 'http://localhost:8000'
+# PORTAL_URL = 'http://localhost:8000' # unused
--- students/stores/processors.py
+++ students/stores/processors.py
@@ -1,5 +1,6 @@
-from django.conf import settings
+# from django.conf import settings
+from django.http import HttpRequest
 
 
 def students_proc(request):
-    return {'PORTAL_URL': settings.PORTAL_URL}
+    return {'PORTAL_URL': HttpRequest.get_raw_uri(request)}
