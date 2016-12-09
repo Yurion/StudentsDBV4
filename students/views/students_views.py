8cc625a3af9c6b4c450a48efc287d8e9ec17b7a8
Yurion <khomykyu@gmail.com>
2016-12-09 18:27:59 +0200

    fix paginator sorting on students model, fix numb sorting

--- students/views/students_views.py
+++ students/views/students_views.py
@@ -1,38 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from ..models import Student
-
-preview_order = 'last_name'
-preview_reverse = ''
 
 
 def students_list(request):
     students = Student.objects.all().order_by('last_name')
 
-    global preview_order
-    global preview_reverse
-
+    # try to order students list
     order_by = request.GET.get('order_by', '')
     if order_by in ('last_name', 'first_name', 'ticket'):
-        preview_order = order_by
         students = students.order_by(order_by)
         if request.GET.get('reverse', '') == '1':
-            preview_reverse = '1'
             students = students.reverse()
-        else:
-            preview_reverse = ''
 
-    if order_by == 'numb':
-        if request.GET.get('reverse', '') == '1':
-            if preview_reverse == '1':
-                students = students.order_by(preview_order)
-            else:
-                students = students.order_by(preview_order).reverse()
-        else:
-            if preview_reverse == '1':
-                students = students.order_by(preview_order).reverse()
-            else:
-                students = students.order_by(preview_order)
+    # paginate students
+    paginator = Paginator(students, 3)
+    page = request.GET.get('page')
+    try:
+        students = paginator.page(page)
+    except PageNotAnInteger:
+        # deliver first page
+        students = paginator.page(1)
+    except EmptyPage:
+        # deliver last page
+        students = paginator.page(paginator.num_pages)
 
     return render(request, 'students/students_list.html', {'students': students, 'request': request})
 
